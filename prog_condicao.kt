8937ae1b336d98e10e54bef3be9105bfff5a5e03
Gilberto Aparecido Bernardo Junior <36751006+gbernardojr@users.noreply.github.com>
2023-10-16 09:20:25 -0300

    Update prog_condicao.kt
    inclusão do comando when

--- prog_condicao.kt
+++ prog_condicao.kt
@@ -1,23 +1,35 @@
 fun main(Args : list){
   Print("Digite o primeiro número: ")
-  var xNumero1 = Readln()
+  var xNumero1 = readln().toDouble()
   Print("Digite o primeiro número: ")
-  var xNumero2 = Readln()
+  var xNumero2 = Readln().toDouble()
 
-  Print("Escolha: 1-Somar, 2-Subtrair, 3-Multiplicar, 4-Dividir)
-  var xEscolha = readln()
+  Print("Escolha: 1-Somar, 2-Subtrair, 3-Multiplicar, 4-Dividir")
+  var xEscolha = readln().toInt()
 
   var xResultado = 0.0
-  if (xEscolha = 1){
+
+/*
+  if (xEscolha == 1){
     xResultado = xNumero1 + xNumero2
-  } else if (xEscolha = 2){
+  } else if (xEscolha == 2){
     xResultado = xNumero1 - xNumero2
-  } else if (xEscolha = 3){
+  } else if (xEscolha == 3){
     xResultado = xNumero1 * xNumero2
-  } else if (xEscolha = 4){
+  } else if (xEscolha == 4){
     xResultado = xNumero1 / xNumero2
   } else {
     Println("Opção Inválida")
   }
+*/
+
+  when (xEscolha){
+    1 -> xResultado = xNumero1 + xNumero2
+    2 -> xResultado = xNumero1 - xNumero2
+    3 -> xResultado = xNumero1 * xNumero2
+    4 -> xResultado = xNumero1 / xNumero2
+    else -> { Println("Opção Inválida") }
+  }
+  
   Println("Resultado da Operação = $xResultado")
 }
